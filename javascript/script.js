36a33d3ba0492b3e04f0159656fc1e3d030ea7e3
Debugger <mbiydzenyuyeileen@gmail.com>
2024-12-03 23:43:36 +0100

    linter fix on javascript file

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,12 @@ fetch(`https://www.swapi.tech/api/films`)
   .then((res) => res.json())
   .then((data) => {
     console.log(data)
-    let charNameOne = document.getElementById('para-one')
-    let charNameTwo = document.getElementById('para-two')
-    let charNameThree = document.getElementById('para-three')
-    let charNameFour = document.getElementById('para-four')
-    let charNameFive = document.getElementById('para-five')
-    let charNameSix = document.getElementById('para-six')
+    const charNameOne = document.getElementById('para-one')
+    const charNameTwo = document.getElementById('para-two')
+    const charNameThree = document.getElementById('para-three')
+    const charNameFour = document.getElementById('para-four')
+    const charNameFive = document.getElementById('para-five')
+    const charNameSix = document.getElementById('para-six')
     for (let index = 0; index < data.result.length; index++) {
       charNameOne.innerHTML = data.result[0].properties.title
       charNameTwo.innerHTML = data.result[1].properties.title
@@ -15,5 +15,5 @@ fetch(`https://www.swapi.tech/api/films`)
       charNameFour.innerHTML = data.result[3].properties.title
       charNameFive.innerHTML = data.result[4].properties.title
       charNameSix.innerHTML = data.result[5].properties.title
-    }
+  }
 })
