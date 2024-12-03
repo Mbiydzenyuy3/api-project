8f164330cbb084b70517318740e33ff98c76c8ab
Debugger <mbiydzenyuyeileen@gmail.com>
2024-12-03 22:14:54 +0100

    adding project content,styling and javascript to the project

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+
+fetch(`https://www.swapi.tech/api/films`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    let charNameOne = document.getElementById('para-one');
+    let charNameTwo = document.getElementById("para-two");
+    let charNameThree = document.getElementById("para-three");
+    let charNameFour = document.getElementById("para-four");
+    let charNameFive = document.getElementById("para-five");
+    let charNameSix = document.getElementById("para-six");
+    for (let index = 0; index < data.result.length; index++) {
+      charNameOne.innerHTML = data.result[0].properties.title
+      charNameTwo.innerHTML = data.result[1].properties.title
+      charNameThree.innerHTML = data.result[2].properties.title
+      charNameFour.innerHTML = data.result[3].properties.title
+      charNameFive.innerHTML = data.result[4].properties.title;
+      charNameSix.innerHTML = data.result[5].properties.title;
+    }
+});
